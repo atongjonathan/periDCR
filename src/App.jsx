5986910d6e57f0d92286f54c903095b957152057
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-09-11 14:40:50 +0300

    refactor: custom preferences

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import React from "react";
-import { Header, Stack } from "@nordhealth/react";
+import { React, useEffect } from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import "./App.css";
-import { Login } from "./Login";
-
+import { Login } from "./components/Login";
+import { SignUp } from "./components/SignUp";
 function App() {
   return (
     <>
-      <Header>
-        <h1 className="n-typescale-l">Nord / React Example</h1>
-      </Header>
-      <main className="n-reset n-stack-horizontal">
-        <Stack className="stack">
-          <Login />
-        </Stack>
-      </main>
+      <BrowserRouter>{/* Decides what component to load based on the url */}
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+        </Routes>
+
+      </BrowserRouter>
     </>
-  );
+
+
+  )
 }
 
-export default App;
+export default App
