4ff09581a7d0d63df4a396f3f14ee279191295e8
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-09-11 09:25:41 +0300

    feat: backend

---
+++ backend/base/api/urls.py
@@ -0,0 +1,13 @@
+from . import views
+from django.urls import path
+from .views import MyTokenObtainView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path("", views.get_routes),
+    path('token/', MyTokenObtainView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
