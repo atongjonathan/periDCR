e04d5e06f48f59716872705b148884ac66b998eb
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-09-13 15:32:40 +0300

    feat: return all user data from token

--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -14,6 +14,11 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         # Add custom claims
         token['username'] = user.username
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['email'] = user.email
+        token['role'] = user.role
+        token['practitioner'] = user.practitioner
         # ...
 
         return token
